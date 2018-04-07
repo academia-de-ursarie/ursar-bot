d5d207ef20fce863521186c97a4a2c4b820c13a2
Victor Rachieru <victor.rachieru@gmail.com>
2018-04-08 00:20:36 +0300

    Refactor weather plugin to support both en and ro outputs

--- ursar/plugins/util/weather.py
+++ ursar/plugins/util/weather.py
@@ -5,15 +5,26 @@ import requests
 
 class WeatherPlugin(UrsarPlugin):
 
-    @respond_to("^(weather|vremea) in (?P<location>.*)$")
+    @respond_to('^weather( in)? (?P<location>.*)$')
     def weather(self, message, location=None):
-        response = requests.get('https://query.yahooapis.com/v1/public/yql?q=select * from weather.forecast where woeid in (select woeid from geo.places(1) where text="%s")&format=json&env=store://datatables.org/alltableswithkeys&u=c' % location)
+        return self.yahoo_weather(location, 'In {location} there are {temperature}°C, {forecast}')
+
+    @respond_to('^vreme(a in)? (?P<location>.*)$')
+    def vremea(self, message, location=None):
+        return self.yahoo_weather(location, 'In {location} sunt {temperature}°C, {forecast}')
+
+    def yahoo_weather(self, location, formatString):
+        query = 'select * from weather.forecast where woeid in (select woeid from geo.places(1) where text="%s") and u="c"' % location
+        response = requests.get('https://query.yahooapis.com/v1/public/yql?q=%s&format=json' % query)
         json = response.json()
 
         if json['query']['results']:
-            currentTemp = int(json['query']['results']['channel']['item']['condition']['temp'])
-            clouds = json['query']['results']['channel']['item']['condition']['text']
-            ttl = (currentTemp - 32) / 1.8
-            return 'In %s sunt %.2f grade, %s' % (location.title(), ttl, clouds)
-        
+            args = {
+                'location': location.title(),
+                'temperature': int(json['query']['results']['channel']['item']['condition']['temp']),
+                'forecast': json['query']['results']['channel']['item']['condition']['text']
+            }
+            
+            return formatString.format(**args)
+
         return None
